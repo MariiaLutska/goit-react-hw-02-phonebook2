0c7424e812e1193cc6534833c737f229c3a59a8b
MariiaLutska <myklush@ukr.net>
2022-08-16 01:04:25 +0300

    json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,36 @@
 import { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Box } from './Box';
+// import PropTypes from 'prop-types';
+// import { Box } from './Box';
 
 export class App extends Component {
   state = {
   contacts: [],
   name: ''
   }
+
+  handleNameChange = event => {
+    console.log(event.currentTarget.value);
+
+    this.setState({inputValue: event.currentTarget.value});
+  };
+
   render() {
     return (
-    <Container>
-      <input
+    // <Container>
+      <form>
+        <label htmlFor="name">Name
+          <input
         type="text"
         value={this.state.name}
-  name="name"
-  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-  required
+        onChange={this.handleNameChange}
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
 />
-    </Container>
+        </label>
+      </form>
+    // </Container>
     
   );
   }
